ac1ac6a3517f0d2ff27bcb0bfffac188e1a0994b
Tyler <tylermwatts@gmail.com>
2019-04-06 14:37:05 -0500

    wip

--- BookLibraryApi/Controllers/BooksController.cs
+++ BookLibraryApi/Controllers/BooksController.cs
@@ -7,21 +7,24 @@ using BookLibraryApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookLibraryApi.Controllers {
+  [Route ("api/[controller]")]
   public class BooksController : ControllerBase {
     Book[] books = new Book[] {
       new Book { Id = 1, Author = "Larson,Erik", Title = "Devil in the White City", Subcategory = Subcategory.True_Crime },
       new Book { Id = 2, Author = "Phillips,Christopher", Title = "Socrates Cafe", Subcategory = Subcategory.Philosophy },
       new Book { Id = 3, Author = "Obama,Michelle", Title = "Becoming", Subcategory = Subcategory.Biography }
     };
+    [HttpGet]
+    public ActionResult<IEnumerable<Book>> Get () {
+      return books;
+    }
 
-    public IEnumerable<Book> GetAllBooks () => books;
-
-    public ActionResult<Book> GetProduct (int id) {
-      var book = books.FirstOrDefault ((b) => b.Id == id);
-      if (book == null) {
-        return NotFound ();
-      }
-      return Ok (book);
+    [HttpGet ("{id}")]
+    public ActionResult<Book> Get (int id) {
+      var book = Array.Find (books, (b) => {
+        return b.Id == id;
+      });
+      return book;
     }
 
   }
